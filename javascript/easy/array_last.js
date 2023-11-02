83e01d1b8859172f7e8d561f5e2a4b842ad174d2
andrewbloese-00 <dev@andrewbloese.me>
2023-11-02 13:30:34 -0700

    added more coverage for the array.last() local test function

--- javascript/easy/array_last.js
+++ javascript/easy/array_last.js
@@ -4,12 +4,28 @@ Array.prototype.last = function (){
 	return this.at(-1)
 }
 
-
+//local test function 
+const IS_TRUE = v => v === true
+const arrays = [
+	[1,2,3],
+	[5,6,7],
+	[],
+	[8,9,10],
+	[11],
+]
+const expected = [
+	3,7,-1,10,11
+]
 function test(){
-	const array = [ 1 , 2 , 3 ]
-	const expected = 3;
-	console.log("Passed: " , array.last() === expected)
+	const results = [];
+	for(let i = 0; i < arrays.length; i++) {
+		results[i] = arrays[i].last() === expected[i];
+	}
+	console.log("Array.last passed:", results.every(IS_TRUE))
 }
+
+
+
 
 test();
 
